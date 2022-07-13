1d9676e95ad4a7f4b99ad53fd54afc864bb5c25d
langermank <langermank@gmail.com>
2022-07-13 14:59:12 -0700

    stuff

--- reality-stock-watch-app/app/routes/show/$show_short_name.jsx
+++ reality-stock-watch-app/app/routes/show/$show_short_name.jsx
@@ -1,19 +1,31 @@
-import seasons from "~/data/season.json";
+// import seasons from "~/data/season.json";
+import supabase from "~/utils/supabase";
 import { useLoaderData, Link, Outlet, useLocation } from "@remix-run/react";
 
 // show_short_name matches file path, change this if actual data changes
-export const loader = async ({ params: { show_short_name } }) => {
-  const season = seasons.filter((c) => c.show_short_name === show_short_name);
+export const loader = async ({ params: { short_name } }) => {
+  //   const season = seasons.filter((c) => c.show_short_name === show_short_name);
 
-  return { season };
+  //   return { season };
+  const { data: seasons, error } = await supabase
+    .from("seasons")
+    .select("name, short_name");
+
+  if (error) {
+    console.log("error,", error.message);
+  }
+
+  return {
+    seasons,
+  };
 };
 
 export default () => {
   const season = useLoaderData();
-
+  console.log(seasons);
   return (
     <div>
-      <p>List all seasons</p>
+      <p>List all seasons from show supabase</p>
       {/* map through season data based on page param */}
       {Object.entries(season).map(([key, value]) => (
         <ul>
--- reality-stock-watch-app/app/routes/show/index.jsx
+++ reality-stock-watch-app/app/routes/show/index.jsx
@@ -1,23 +1,24 @@
-import { useLoaderData, Link } from "@remix-run/react";
-import shows from "~/data/show.json";
+// dont think we need this index
+// import { useLoaderData, Link } from "@remix-run/react";
+// import shows from "~/data/show.json";
 
-export const loader = async () => {
-  return {
-    shows,
-  };
-};
+// export const loader = async () => {
+//   return {
+//     shows,
+//   };
+// };
 
-export default function Index() {
-  const { shows } = useLoaderData();
+// export default function Index() {
+//   const { shows } = useLoaderData();
 
-  return (
-    <div>
-      <h1>List all shows</h1>
-      {shows.map((show) => (
-        <p key={show.id}>
-          <Link to={`${show.short_name}`}>{show.name}</Link>
-        </p>
-      ))}     
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>List all shows</h1>
+//       {shows.map((show) => (
+//         <p key={show.id}>
+//           <Link to={`${show.short_name}`}>{show.name}</Link>
+//         </p>
+//       ))}
+//     </div>
+//   );
+// }
