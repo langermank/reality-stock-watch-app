cf52c244d1f682783fc5e34b6851a44c71ee6d5c
Katie Langerman <langermank@github.com>
2022-10-12 23:05:41 +0000

    comment out postcss config temp

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,20 @@
 const atImport = require("postcss-import");
 const atImportGlob = require("postcss-import-ext-glob");
 // const autoprefixer = require("autoprefixer");
-const presetEnv = require("postcss-preset-env");
+// const presetEnv = require("postcss-preset-env");
 
 module.exports = {
   plugins: [
     atImportGlob(),
     atImport(),
-    presetEnv({
-      stage: 3,
-      // https://preset-env.cssdb.org/
-      features: {
-        "nesting-rules": true,
-        "has-pseudo-class": true,
-      },
-    }),
+    // presetEnv({
+    //   stage: 3,
+    //   // https://preset-env.cssdb.org/
+    //   features: {
+    //     "nesting-rules": true,
+    //     "has-pseudo-class": true,
+    //   },
+    // }),
   ],
 };
 
