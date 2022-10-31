9bc52b1116f52fb03a39b98863eddbd1b34914f1
yhyky <57417299+yhyky@users.noreply.github.com>
2022-10-30 22:33:39 -0700

    prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    requirePragma: false,
+    tabWidth: 2,
+    useTabs: false,
+    overrides: [
+        {
+            files: '*.js',
+            options: {
+                semi: true,
+                singleQuote: true,
+                quoteProps: 'as-needed',
+                trailingComma: 'all',
+            },
+        }, {
+            files: '*.html',
+            options: {
+                bracketSameLine: false,
+                arrowParens: 'always', 
+            }
+        }, {
+            files: '.prettierrc.js',
+            options: {
+                parser: 'js',
+            },
+        }
+    ],
+};
