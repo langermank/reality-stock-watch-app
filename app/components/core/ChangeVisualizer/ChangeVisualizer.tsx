8cbb1aa25ebee2da8ad166786641dad5d951d7b4
langermank <18661030+langermank@users.noreply.github.com>
2022-12-06 16:26:49 -0800

    fix icon

--- app/components/core/ChangeVisualizer/ChangeVisualizer.tsx
+++ app/components/core/ChangeVisualizer/ChangeVisualizer.tsx
@@ -8,9 +8,9 @@ export type ChangeVisualizerProps = {
 
 export function ChangeVisualizer({ label, changeState = "nochange" }: ChangeVisualizerProps) {
   const icon = () => {
-    if ((changeState = "increase")) {
+    if (changeState === "increase") {
       return <ArrowSquareUp weight="fill" />;
-    } else if ((changeState = "decrease")) {
+    } else if (changeState === "decrease") {
       return <ArrowSquareDown weight="fill" />;
     } else {
       return <DiceOne weight="fill" />;
@@ -18,12 +18,8 @@ export function ChangeVisualizer({ label, changeState = "nochange" }: ChangeVisu
   };
   return (
     <div className="ChangeVisualizer" data-state={changeState}>
-      {/* TODO: why need a wrap here? */}
-      <>
-        {/* <Icon changeState={changeState} /> */}
-        {icon()}
-        {label && <p className="ChangeVisualizer-label">{label}</p>}
-      </>
+      {icon()}
+      {label && <p className="ChangeVisualizer-label">{label}</p>}
     </div>
   );
 }
