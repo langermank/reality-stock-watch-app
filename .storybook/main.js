d5f872c63141509af2ee84669aaecd8ec5d45b82
langermank <18661030+langermank@users.noreply.github.com>
2022-12-11 13:02:00 -0800

    setup fonts in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 module.exports = {
-  stories: ["../app/**/*.stories.mdx", "../app/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../app/**/*.stories.mdx', '../app/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  staticDirs: ['../public'],
+  framework: '@storybook/react',
 };
